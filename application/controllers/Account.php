08334df395afeeb30923890756967887f20d16bd
newafgani97 <newafgani97@gmail.com>
2019-10-12 04:48:14 +0700

    Login & lupa pas setengah jadi

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -5,7 +5,17 @@ class Account extends CI_Controller {
 
 	public function loginAccount()
 	{
-
+		$this->load->model('m_login');
+		if($this->m_login->login($this->input->post('email'), $this->input->post('password'))==TRUE){
+			echo "<script>  
+		 			alert('Login Success!');
+                 </script>";
+			$this->load->view('V_homePage');
+		}else{
+			echo "<script>
+                    alert('Login Failed, Wrong Username or Password');
+                 </script>";
+		}
 	}
 
 	public function authentication()
@@ -20,7 +30,7 @@ class Account extends CI_Controller {
 
 	public function setPassword()
 	{
-
+		$this->load->model('M_login');
 	}
 
 	public function registData()
