e4bc75b45c57cbb90074388ad85a619c5b994408
Robertus Dwi Ari Utomo <robert.unix98@gmail.com>
2019-11-23 20:11:04 +0700

    Css update

--- application/views/V_registerPage.php
+++ application/views/V_registerPage.php
@@ -2,15 +2,11 @@
 <html lang="en">
 
 <head>
-    <!-- Stylesheets -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>css/w3.css" />
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 
 <body>
 <!-- checkout section  -->
 <section class="checkout-section spad">
-
     <button class="tablink" onclick="openPage('Personal Account', this, 'red')">Personal Account</button>
     <button class="tablink" onclick="openPage('Buisness Account', this, 'green')" id="defaultOpen">Buisness Account</button>
 
@@ -72,13 +68,13 @@
                                 <input type="text" placeholder="Institution Address">
                             </div>
                             <div class="col-md-5">
-                                <div class="col-md-12" style="bottom" : 22px;>
-                                    <label for="sel1">Select list (select one):</label>
+                                <div class="col-md-12" style='bottom : 22px;'>
+                                    <label for="sel1">Institution type: </label>
                                     <select class="form-control" id="sel1">
-                                        <option>Institution type 1</option>
-                                        <option>Institution type 2</option>
-                                        <option>Institution type 3</option>
-                                        <option>Institution type 4</option>
+                                        <option> 1</option>
+                                        <option> 2</option>
+                                        <option> 3</option>
+                                        <option> 4</option>
                                     </select>
                                 </div>
                             </div>
@@ -92,12 +88,10 @@
         </div>
     </div>
     </div>
-
 </section>
 
 <!--====== Javascripts & Jquery ======-->
 <script src="<?php echo base_url(); ?>js/tabs.js"></script>
-<script src="<?php echo base_url(); ?>js/registerw3.js"></script>
 
 </body>
 
