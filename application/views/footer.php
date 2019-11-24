5675098793c4086b45ac78cd9ca1a8d4e4288d3b
Robertus Dwi Ari Utomo <robert.unix98@gmail.com>
2019-11-24 10:47:16 +0700

    footer update

--- application/views/footer.php
+++ application/views/footer.php
@@ -43,40 +43,38 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 col-sm-6 col-xs-12">
-                    <div class="footer-content">
-                        <div class="footer-head text-center">
-                            <h4>-----Connect With Us-----</h4>
+                <div class="col-md-3 col-sm-3 col-xs-12">
+                    <div class="footer-content text-center">
+                        <div class="footer-head">
+                            <h4>--Connect With Us--</h4>
                         </div>
-                        <div class="col-md-6 col-sm-6 col-xs-12 footer-contacts text-center">
                             <p><span>CS:</span> +62 (21) xxxx xxxx</p>
                             <p><span>Sales:</span> +62 (21) xxxx xxxx</p>
-                        </div>
-                        <div class="col-md-6 col-sm-6 col-xs-12 footer-contacts">
-                            <div class="form contact-form">
-                                <div id="sendmessage">Your message has been sent. Thank you!</div>
-                                <div id="errormessage"></div>
-                                <form action="" method="post" role="form" class="contactForm">
-                                    <div class="form-group">
-                                        <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                        <div class="validation"></div>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                                        <div class="validation"></div>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                                        <div class="validation"></div>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                                        <div class="validation"></div>
-                                    </div>
-                                    <div class="text-center"><button type="submit">Send Message</button></div>
-                                </form>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 col-xs-12 footer-contacts">
+                    <div class="form contact-form">
+                        <div id="sendmessage">Your message has been sent. Thank you!</div>
+                        <div id="errormessage"></div>
+                        <form action="" method="post" role="form" class="contactForm">
+                            <div class="form-group">
+                                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                <div class="validation"></div>
                             </div>
-                        </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                <div class="validation"></div>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                                <div class="validation"></div>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                                <div class="validation"></div>
+                            </div>
+                            <div class="text-center"><button type="submit">Send Message</button></div>
+                        </form>
                     </div>
                 </div>
             </div>
