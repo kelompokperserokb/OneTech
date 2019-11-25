8b95027d947c624257c5362f7d2b72f3b636f0a0
Robertus Dwi Ari Utomo <robert.unix98@gmail.com>
2019-11-25 12:32:42 +0700

    Update product Page

--- application/views/V_productPage.php
+++ application/views/V_productPage.php
@@ -5,6 +5,7 @@
 </head>
 
 <body>
+
 <!-- product section -->
 <section class="product-section">
 	<div class="container">
@@ -27,17 +28,14 @@
 			</div>
             <?php echo '
 			<div class="col-lg-6 product-details">
-				<h2 class="p-title">'.$product["data_array"][0]->product_name.' - '.$product["data_array"][0]->product_type.'</h2>
+				<h2 class="p-title">'.$product["data_array"][0]->product_name.'</h2>
 				<h3 class="p-price">Rp. '.number_format($product["data_array"][0]->product_price,2,",",".").'</h3>
-				<h4 class="p-stock">Available: <span>'. $stock_status .'<input type="hidden" id ="stock_quota" value="'.$stock.'" /></span></h4>
+				<!--<h4 class="p-stock">Available: <span>'. $stock_status .'<input type="hidden" id ="stock_quota" value="'.$stock.'" /></span></h4>-->
 				<!--<div class="p-review">
 					<a href="">3 reviews</a>|<a href="">Add your review</a>
 				</div>-->
-				<div class="quantity">
-					<p>Quantity</p>
-					<div class="pro-qty"><input type="text" value="1" name="quantity"></div>
-				</div>
-				<a href="#" class="site-btn">SHOP NOW</a>
+				
+				<!--<a href="#" class="site-btn">SHOP NOW</a>-->
 				<div id="accordion" class="accordion-area">
 					<div class="panel">
 						<div class="panel-header" id="headingOne">
@@ -94,7 +92,8 @@
 			<th>Product Code</th>
 			<th>Product Type</th>
 			<th>Quota</th>
-			<th>Description Typr</th>
+			<th>Description Type</th>
+            <th>Quantity</th>
 			<th>order</th>
 		</tr>
         <?php for ($i = 0 ; $i < $product_type["count"] ; $i++ ){
@@ -104,226 +103,18 @@
 			<td>'.$product_type["data_array"][$i]->product_type.'</td>
 			<td>'.$product_type["data_array"][$i]->quota.'</td>
 			<td>'.$product_type["data_array"][$i]->description_type.'</td>
-			<td><a href="'.base_url().'product/'.$product["data_array"][0]->product_id.'/'.$product_type["data_array"][$i]->type_id.'">order</a></td>
+			<td ><div class="quantity">
+					<p>Quantity</p>
+					<div class="pro-qty"><input type="text" value="1" name="quantity"></div>
+				</div></td>
+			<!--<td><a href="'.base_url().'product/'.$product["data_array"][0]->product_id.'/'.$product_type["data_array"][$i]->type_id.'">order</a></td>-->
+			<td><a href="#">order</a></td>
 		</tr>';} ?>
 
 	</table>
 </div>
 <!-- end mini table-->
 
-<!-- RELATED PRODUCTS section -->
-<section class="related-product-section">
-	<div class="container">
-		<div class="section-title">
-			<h2>RELATED PRODUCTS</h2>
-		</div>
-		<div class="product-slider owl-carousel">
-			<div class="product-item">
-				<div class="pi-pic">
-					<img src="<?php echo base_url(); ?>Asset//img/tandatanya.jpg" alt="">
-					<div class="pi-links">
-						<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-					</div>
-				</div>
-				<div class="pi-text">
-					<h6>$35,00</h6>
-					<p>Flamboyant Pink Top </p>
-				</div>
-			</div>
-			<div class="product-item">
-				<div class="pi-pic">
-					<div class="tag-new">New</div>
-					<img src="<?php echo base_url(); ?>Asset//img/tandatanya.jpg" alt="">
-					<div class="pi-links">
-						<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-					</div>
-				</div>
-				<div class="pi-text">
-					<h6>$35,00</h6>
-					<p>Black and White Stripes Dress</p>
-				</div>
-			</div>
-			<div class="product-item">
-				<div class="pi-pic">
-					<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-					<div class="pi-links">
-						<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-					</div>
-				</div>
-				<div class="pi-text">
-					<h6>$35,00</h6>
-					<p>Flamboyant Pink Top </p>
-				</div>
-			</div>
-			<div class="product-item">
-				<div class="pi-pic">
-					<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-					<div class="pi-links">
-						<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-					</div>
-				</div>
-				<div class="pi-text">
-					<h6>$35,00</h6>
-					<p>Flamboyant Pink Top </p>
-				</div>
-			</div>
-			<div class="product-item">
-				<div class="pi-pic">
-					<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-					<div class="pi-links">
-						<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-					</div>
-				</div>
-				<div class="pi-text">
-					<h6>$35,00</h6>
-					<p>Flamboyant Pink Top </p>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<!-- RELATED PRODUCTS section end -->
-<!-- Product filter section -->
-<section class="product-filter-section">
-	<div class="container">
-		<div class="section-title">
-			<h2>BROWSE TOP SELLING PRODUCTS</h2>
-		</div>
-
-		<div class="row">
-			<div class="col-lg-3 col-sm-6">
-				<div class="product-item">
-					<div class="pi-pic">
-						<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-						<div class="pi-links">
-							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-						</div>
-					</div>
-					<div class="pi-text">
-						<h6>$35,00</h6>
-						<p>Flamboyant Pink Top </p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3 col-sm-6">
-				<div class="product-item">
-					<div class="pi-pic">
-						<div class="tag-sale">ON SALE</div>
-						<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-						<div class="pi-links">
-							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-						</div>
-					</div>
-					<div class="pi-text">
-						<h6>$35,00</h6>
-						<p>Black and White Stripes Dress</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3 col-sm-6">
-				<div class="product-item">
-					<div class="pi-pic">
-						<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-						<div class="pi-links">
-							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-						</div>
-					</div>
-					<div class="pi-text">
-						<h6>$35,00</h6>
-						<p>Flamboyant Pink Top </p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3 col-sm-6">
-				<div class="product-item">
-					<div class="pi-pic">
-						<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-						<div class="pi-links">
-							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-						</div>
-					</div>
-					<div class="pi-text">
-						<h6>$35,00</h6>
-						<p>Flamboyant Pink Top </p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3 col-sm-6">
-				<div class="product-item">
-					<div class="pi-pic">
-						<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-						<div class="pi-links">
-							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-						</div>
-					</div>
-					<div class="pi-text">
-						<h6>$35,00</h6>
-						<p>Flamboyant Pink Top </p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3 col-sm-6">
-				<div class="product-item">
-					<div class="pi-pic">
-						<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-						<div class="pi-links">
-							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-						</div>
-					</div>
-					<div class="pi-text">
-						<h6>$35,00</h6>
-						<p>Black and White Stripes Dress</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3 col-sm-6">
-				<div class="product-item">
-					<div class="pi-pic">
-						<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-						<div class="pi-links">
-							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-						</div>
-					</div>
-					<div class="pi-text">
-						<h6>$35,00</h6>
-						<p>Flamboyant Pink Top </p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3 col-sm-6">
-				<div class="product-item">
-					<div class="pi-pic">
-						<img src="<?php echo base_url(); ?>Asset/img/tandatanya.jpg" alt="">
-						<div class="pi-links">
-							<a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
-
-						</div>
-					</div>
-					<div class="pi-text">
-						<h6>$35,00</h6>
-						<p>Flamboyant Pink Top </p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="text-center pt-5">
-			<button class="site-btn sb-line sb-dark">LOAD MORE</button>
-		</div>
-	</div>
-</section>
-<!-- Product filter section end -->
 
 <script src="<?php echo base_url(); ?>Asset/js/main.js"></script>
 </body>
