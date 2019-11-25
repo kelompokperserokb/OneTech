24254074f0ff458eb0348750928beb02807b3ca5
newafgani97 <newafgani97@gmail.com>
2019-11-25 09:55:33 +0700

    Slider Image, Slider, how to order fix, change head

--- application/views/V_howToOrder.php
+++ application/views/V_howToOrder.php
@@ -2,13 +2,13 @@
 <html lang="zxx">
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content=" Divisima | eCommerce Template">
-    <meta name="keywords" content="divisima, eCommerce, creative, html">
+    <meta name="description" content="OneTech, Your Mining Solution Service">
+    <meta name="keywords" content="OneTech, Mining">
 
     <link rel="stylesheet" href="<?php echo base_url(); ?>Asset/css/timeline.css"/>
 </head>
 <body>
-<div>
+<div class="timeline">
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste iusto quas eligendi corporis id eius corrupti temporibus velit? Molestias harum voluptatibus veritatis explicabo ut velit deserunt, saepe sit commodi?</p>
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste iusto quas eligendi corporis id eius corrupti temporibus velit? Molestias harum voluptatibus veritatis explicabo ut velit deserunt, saepe sit commodi?</p>
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste iusto quas eligendi corporis id eius corrupti temporibus velit? Molestias harum voluptatibus veritatis explicabo ut velit deserunt, saepe sit commodi?</p>
