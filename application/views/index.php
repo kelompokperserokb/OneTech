239030040d2128f6a9bbe5cb42f8a239b4e81874
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2020-01-07 19:21:26 +0700

    Update bootstrap.min.css
    Karena wedhus aja ngembek

--- application/views/index.php
+++ application/views/index.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>Asset/css/bootstrap-337.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>Asset/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>Asset/css/style.css">
 </head>
